729a090b5545ff1928eaa8bb5a27214969891bb2
Jonatan Nieto <jonatannietoa@gmail.com>
2025-07-16 00:24:27 +0200

    first commit

---
+++ src/main/java/co/unaicorn/shipfyze/ShipfyzeApplication.java
@@ -0,0 +1,13 @@
+package co.unaicorn.shipfyze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipfyzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipfyzeApplication.class, args);
+	}
+
+}
