de08b7defe26aa3a3dc3671661882e23a30257fe
Jonatan Nieto <jonatannietoa@gmail.com>
2025-07-17 01:00:42 +0200

    Create src/main/java/co/unaicorn/shipfyze/products/domain/exceptions/ProductNotFoundException.java

---
+++ src/main/java/co/unaicorn/shipfyze/products/domain/exceptions/ProductNotFoundException.java
@@ -0,0 +1,8 @@
+package co.unaicorn.shipfyze.products.domain.exceptions;
+
+public class ProductNotFoundException extends RuntimeException {
+
+    public ProductNotFoundException(String id) {
+        super("Product with id " + id + " not found");
+    }
+}
